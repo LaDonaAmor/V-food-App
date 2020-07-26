186eca67782082fb23a57ffad7f47920c9ff3890
La Doña <65674047+Ifeoluwa02@users.noreply.github.com>
2020-07-27 00:39:28 +0100

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,30 +1,36 @@
-// Declearing Variables
-const days = document.getElementById("days");
-const hours = document.getElementById("hours");
-const minutes = document.getElementById("minutes");
-const seconds = document.getElementById("seconds");
-// Set Date counting down to
-const countDownDate = new Date("AUG 5, 2020 15:37:25").getTime();
-// Update the count down every 1 second
-const time = setInterval(function () {
-// Get today's date and time
-const now = new Date().getTime();
-// Find the distance between now and the count down date
-const distance = countDownDate - now;
-// Time calculations for days, hours, minutes and seconds
-const day = Math.floor(distance / (1000 * 60 * 60 * 24));
-const hour = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-const minute = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-const second = Math.floor((distance % (1000 * 60)) / 1000);
-// OutPut Results
-days.innerHTML = day;
-hours.innerHTML = hour;
-minutes.innerHTML = minute;
-seconds.innerHTML = second;
-// If the count down is over, write some text 
-if (distance 
-< 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+function getTimeRemaining(endtime) {
+const total = Date.parse(endtime) - Date.parse(new Date());
+const seconds = Math.floor((total / 1000) % 60);
+const minutes = Math.floor((total / 1000 / 60) % 60);
+const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+const days = Math.floor(total / (1000 * 60 * 60 * 24));
+return {
+total,
+days,
+hours,
+minutes,
+seconds
+};
+}
+function initializeClock(id, endtime) {
+const clock = document.getElementById(id);
+const daysSpan = clock.querySelector('.days');
+const hoursSpan = clock.querySelector('.hours');
+const minutesSpan = clock.querySelector('.minutes');
+const secondsSpan = clock.querySelector('.seconds');
+function updateClock() {
+const t = getTimeRemaining(endtime);
+daysSpan.innerHTML = t.days;
+hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+if (t.total 
+<= 0) {
+    clearInterval(timeinterval);
     }
-    }, 1000);
+    }
+    updateClock();
+    const timeinterval = setInterval(updateClock, 1000);
+    }
+    const deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+    initializeClock('clockdiv', deadline);
