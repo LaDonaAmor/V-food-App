b4408b9509eabb6d1fdfa9f9f1bbbee164aa40f7
La Doña <65674047+Ifeoluwa02@users.noreply.github.com>
2020-07-28 15:30:03 +0100

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,36 +1,101 @@
 function getTimeRemaining(endtime) {
-const total = Date.parse(endtime) - Date.parse(new Date());
-const seconds = Math.floor((total / 1000) % 60);
-const minutes = Math.floor((total / 1000 / 60) % 60);
-const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
-const days = Math.floor(total / (1000 * 60 * 60 * 24));
-return {
-total,
-days,
-hours,
-minutes,
-seconds
-};
-}
-function initializeClock(id, endtime) {
-const clock = document.getElementById(id);
-const daysSpan = clock.querySelector('.days');
-const hoursSpan = clock.querySelector('.hours');
-const minutesSpan = clock.querySelector('.minutes');
-const secondsSpan = clock.querySelector('.seconds');
-function updateClock() {
-const t = getTimeRemaining(endtime);
-daysSpan.innerHTML = t.days;
-hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-if (t.total 
-<= 0) {
-    clearInterval(timeinterval);
-    }
-    }
-    updateClock();
-    const timeinterval = setInterval(updateClock, 1000);
-    }
-    const deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
-    initializeClock('clockdiv', deadline);
+  const total = Date.parse(endtime) - Date.parse(new Date());
+  const seconds = Math.floor((total / 1000) % 60);
+  const minutes = Math.floor((total / 1000 / 60) % 60);
+  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(total / (1000 * 60 * 60 * 24));
+  return {
+  total,
+  days,
+  hours,
+  minutes,
+  seconds
+  };
+  }
+  function initializeClock(id, endtime) {
+  const clock = document.getElementById(id);
+  const daysSpan = clock.querySelector('.days');
+  const hoursSpan = clock.querySelector('.hours');
+  const minutesSpan = clock.querySelector('.minutes');
+  const secondsSpan = clock.querySelector('.seconds');
+  function updateClock() {
+  const t = getTimeRemaining(endtime);
+  daysSpan.innerHTML = t.days;
+  hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+  minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+  secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+  if (t.total 
+  <= 0) {
+      clearInterval(timeinterval);
+      }
+      }
+      updateClock();
+      const timeinterval = setInterval(updateClock, 1000);
+      }
+      const deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+      initializeClock('clockdiv', deadline);
+  
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml2');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml2 .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
+  }).add({
+    targets: '.ml2',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+  
+  var textWrapper = document.querySelector('.ml9 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml9 .letter',
+    scale: [0, 1],
+    duration: 1500,
+    elasticity: 600,
+    delay: (el, i) => 45 * (i+1)
+  }).add({
+    targets: '.ml9',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+  var textWrapper = document.querySelector('.ml11 .letters');
+  textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+  anime.timeline({loop: true})
+    .add({
+      targets: '.ml11',
+      scaleY: [0,1],
+      opacity: [0.5,1],
+      easing: "easeOutExpo",
+      duration: 700
+    })
+    .add({
+      targets: '.ml11 .letter',
+      opacity: [0,1],
+      easing: "easeOutExpo",
+      duration: 600,
+      offset: '-=775',
+      delay: (el, i) => 34 * (i+1)
+    }).add({
+      targets: '.ml11',
+      opacity: 0,
+      duration: 1000,
+      easing: "easeOutExpo",
+      delay: 1000
+    });
+   
